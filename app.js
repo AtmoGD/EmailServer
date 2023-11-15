d0da75a1f83ff9962494e3d4d5e894a46110fdee
Dennis Hawran <dennishawran@gmail.com>
2023-11-15 19:58:18 +0100

    Init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => RequestConcept(req, res));
+
+async function RequestConcept(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const name = req.query.name;
+    const email = req.query.email;
+    const message = req.query.message;
+
+    console.log("NEW ENTRY: " + name + email + message);
+
+    res.type('application/json').send(response);
+}
