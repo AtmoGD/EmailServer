1edb3ba8a3d03056ec8685da0473c6c714f27b17
Dennis Hawran <dennishawran@gmail.com>
2023-11-15 20:26:50 +0100

    Dunno again

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
+const nodeMailer = require('nodemailer');
+
 const app = express();
 const port = process.env.PORT || 3001;
-
-var nodeMailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'gdatmo@gmail.com',
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
 
 app.get("/", (req, res) => RequestConcept(req, res));
 
 async function RequestConcept(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
 
-
     const name = req.query.name;
     const email = req.query.email;
     const message = req.query.message;
 
     const response = "From: " + name + "\nEmail: " + email + "\nMessage: " + message;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'gdatmo@gmail.com',
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
 
     var mailOptions = {
         from: 'gdatmo@gmail.com',
